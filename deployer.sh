520bd15d7382b70308dad89d5202e8dfb49f734a
Amber Hsu <amberhsu9557@gmail.com>
2022-04-11 09:44:59 +0800

    Update deployer.sh

--- deployer.sh
+++ deployer.sh
@@ -8,27 +8,10 @@ app_init () {
     docker-compose up -d transferapp
     docker-compose exec transferdb /bin/bash -c 'until psql -h localhost -U postgres -c "\q" ; do sleep 1; done'
 
-    # echo -e "Create database\n"
-
-    # echo -e "Reset schema ...\n"
-    # docker-compose exec -T backend /bin/bash -c 'django-admin reset_schema --noinput'
-
-    # echo -e "Keep original transferdb data ...\n"
-    # [ -d "./data/transferdb" ] && [ ! -f "./data/transferdb/backup/transferdb_`date +'%Y_%m_%d'`.tar" ] && cd ./data/transferdb && mkdir -p backup && sudo tar cvf "transferdb_`date +'%Y_%m_%d'`.tar" data 1>/dev/null && mv "transferdb_`date +'%Y_%m_%d'`.tar" backup/ && cd - > /dev/null
-
     echo -e "Migrate ...\n"
     docker-compose exec transferapp flask db init
     docker-compose exec transferapp flask db migrate
     docker-compose exec -T transferapp flask db upgrade
-    # docker-compose exec -T transferapp flask db downgrade
-
-    # echo -e "Collectstatic ...\n"
-    # docker-compose exec -T backend /bin/bash -c 'django-admin collectstatic --noinput'
-
-    # echo -e "Load fixtures ...\n"
-    # substitute_fixture_template
-    # docker-compose exec -T backend /bin/bash -c 'django-admin loaddata /app/fixtures/default-*.json'
-    # docker-compose exec -T backend /bin/bash -c 'rm -f /app/fixtures/default-*.{json,yml}'
 }
 
 
@@ -38,7 +21,6 @@ shift
 
 case $subcommand in
   gen ) # generate env files
-    cp ./apps/transferservice/src/config/.env.sample ./apps/transferservice/src/config/.env
     cp ./dockeryml/docker-compose.yml docker-compose.yml
     rm -f .env
     echo "COMPOSE_PROFILES='${COMPOSE_PROFILES}'" >> .env
@@ -47,9 +29,7 @@ case $subcommand in
   reset|init ) # reset database and start with fixtures
     bash ./deployer.sh gen
     bash ./deployer.sh down
-    
     app_init
-
     bash ./deployer.sh up
     ;;
   build ) # build image without cache
